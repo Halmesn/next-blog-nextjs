8afda52bdb4613bf3b7fa2a02dc3a6eaa08f344c
Adrian Li <xiaxi.li.syd@gmail.com>
2021-04-28 20:07:36 +1000

    add contents to home page hero section

--- components/home/HeroSection.js
+++ components/home/HeroSection.js
@@ -1,5 +1,19 @@
 import styles from './HeroSection.module.css';
+import Image from 'next/image';
 
 export default function HeroSection() {
-  return <section className={styles.hero}></section>;
+  return (
+    <section className={styles.hero}>
+      <div className={styles.image}>
+        <Image
+          src="/images/site/avatar.jpg"
+          alt="an image showing Adrian"
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Hi, I'm Adrian</h1>
+      <p>I built this simple blog when I was learning and practicing NextJs.</p>
+    </section>
+  );
 }
