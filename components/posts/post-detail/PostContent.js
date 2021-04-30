e5636e3837fe5863d9c508a8f93cd36b07909ac9
Adrian Li <xiaxi.li.syd@gmail.com>
2021-04-30 16:32:57 +1000

    reduce build file size

--- components/posts/post-detail/PostContent.js
+++ components/posts/post-detail/PostContent.js
@@ -2,8 +2,13 @@ import Image from 'next/image';
 import ReactMarkdown from 'react-markdown';
 import styles from './PostContent.module.css';
 import PostHeader from './PostHeader';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+import atomDark from 'react-syntax-highlighter/dist/cjs/styles/prism/atom-dark';
+import js from 'react-syntax-highlighter/dist/cjs/languages/prism/javascript';
+import css from 'react-syntax-highlighter/dist/cjs/languages/prism/css';
+
+SyntaxHighlighter.registerLanguage('js', js);
+SyntaxHighlighter.registerLanguage('css', css);
 
 export default function PostContent({ post }) {
   const { content, title, image, slug } = post;
